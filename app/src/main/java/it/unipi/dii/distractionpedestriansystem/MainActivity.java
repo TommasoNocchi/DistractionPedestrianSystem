58b1e0b5c058481972813c95b671f7def62b3f00
Tommaso <tommaso.nocchi@gmail.com>
2022-05-06 17:38:51 +0200

    Commit 3, Starting the configuration to interface with the Beacon

--- app/src/main/java/it/unipi/dii/distractionpedestriansystem/MainActivity.java
+++ app/src/main/java/it/unipi/dii/distractionpedestriansystem/MainActivity.java
@@ -1,6 +1,11 @@
 package it.unipi.dii.distractionpedestriansystem;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -8,23 +13,39 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.kontakt.sdk.android.common.profile.RemoteBluetoothDevice;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+import it.unipi.dii.distractionpedestriansystem.service.BackgroundScanService;
+
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener,  View.OnClickListener{
 
     private SensorManager sm;
     private Sensor s1;
     private Sensor s2;
     private static String TAG = "PedestrianDistractionSystem";
 
+
+    private Intent serviceIntent;
+    private TextView statusText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        serviceIntent = new Intent(getApplicationContext(), BackgroundScanService.class);
+
         // Setup sensors
         sensorSetup();
+
+        //Setup buttons
+        setupButtons();
     }
 
     @Override
@@ -39,8 +60,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     protected void onResume() {
         super.onResume();
+
+        //Usati per LinearAcceleration and StepDetector (*)
         sm.registerListener(this, s1, SensorManager.SENSOR_DELAY_GAME);
         sm.registerListener(this, s2, SensorManager.SENSOR_DELAY_GAME);
+
+        //Register Broadcast receiver that will accept results from background scanning (**)
+        IntentFilter intentFilter = new IntentFilter(BackgroundScanService.ACTION_DEVICE_DISCOVERED);
+        registerReceiver(scanningBroadcastReceiver, intentFilter);
+    }
+
+    protected void onPause() {
+        unregisterReceiver(scanningBroadcastReceiver);
+        super.onPause();
     }
 
     @Override
@@ -57,4 +89,40 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             finish();
         }
     }
+
+    private void setupButtons() {
+        Button startScanButton = (Button) findViewById(R.id.start_scan_button);
+        Button stopScanButton = (Button) findViewById(R.id.stop_scan_button);
+        startScanButton.setOnClickListener(this);
+        stopScanButton.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.start_scan_button:
+                startBackgroundService();
+                break;
+            case R.id.stop_scan_button:
+                stopBackgroundService();
+                break;
+        }
+    }
+
+    private void startBackgroundService() {
+        startService(serviceIntent);
+    }
+
+    private void stopBackgroundService() {
+        stopService(serviceIntent);
+    }
+
+    private final BroadcastReceiver scanningBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //Device discovered!
+            int devicesCount = intent.getIntExtra(BackgroundScanService.EXTRA_DEVICES_COUNT, 0);
+            RemoteBluetoothDevice device = intent.getParcelableExtra(BackgroundScanService.EXTRA_DEVICE);
+            statusText.setText(String.format("Total discovered devices: %d\n\nLast scanned device:\n%s", devicesCount, device.toString()));
+        }
+    };
 }
